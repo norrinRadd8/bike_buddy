eee52378c00fed8074b092f8928026880443d367
Michael Yeates <michael-yeates@outlook.com>
2023-01-08 03:41:44 +0000

    Add animation to route line, re-factor some code

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,39 +2,32 @@
 var map = L.map("map").setView([51.505, -0.09], 13);
 var startLocation;
 var endLocation;
-var routeLine;
 var startMarker;
 var endMarker;
-var apiKey = "7kW5591HWQBXAVyMwGHUlDFNjWbvrhTF";
-var baseURL = "https://api.tomtom.com/routing/1/calculateRoute/";
-
 var isRouteDrawn = false;
 var mapLayer = MQ.mapLayer(),
   map;
-// VARIABLES FOR AQI (Air Quality Index)
 
+// VARIABLES FOR AQI (Air Quality Index)
 var url = "http://api.waqi.info/feed/";
 var url2 = "/?token=2541043dcded3bc723e5446a29135ac1523b1111";
 var city = "London";
 
-
 function displayMap() {
   // Add a tile layer to the map
-  L.tileLayer(
-    "https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}",
-    {
-      attribution:
-        '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      minZoom: 0,
-      maxZoom: 22,
-      subdomains: "abcd",
-      accessToken:
-        "ukHSBSSJvmHto21L0igMtBEPPz5BXrFd7buaLp6nOHvXHev79gRJKl3oqjqK2e0y",
-    }
-  ).addTo(map);
+  L.tileLayer("https://{s}.tile.jawg.io/jawg-terrain/{z}/{x}/{y}{r}.png?access-token={accessToken}", {
+    attribution:
+      '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    minZoom: 0,
+    maxZoom: 22,
+    subdomains: "abcd",
+    accessToken: "ukHSBSSJvmHto21L0igMtBEPPz5BXrFd7buaLp6nOHvXHev79gRJKl3oqjqK2e0y",
+  }).addTo(map);
 }
 
 function getRouteData(startLocation, endLocation) {
+  var apiKey = "7kW5591HWQBXAVyMwGHUlDFNjWbvrhTF";
+  var baseURL = "https://api.tomtom.com/routing/1/calculateRoute/";
   var calculateRouteURL = `${baseURL}${startLocation}:${endLocation}/json?key=${apiKey}&travelMode=bicycle&traffic=true&routeType=thrilling&hilliness=low&avoid=motorways&avoid=tollRoads&avoid=ferries&avoid=unpavedRoads`;
   // Info for params > travelMode plots route with bicycle lanes if pos + traffic plots route with least traffic + routeType allows the use of hilliness to plot routes with low elevation + avoid motorways, tollRoads, ferries & unpavedRoads
 
@@ -43,6 +36,7 @@ function getRouteData(startLocation, endLocation) {
     .then(function (routeData) {
       // Pass route data to displayRoute()
       displayRoute(routeData);
+      displayRouteDetails(routeData);
     })
     .catch(function (error) {
       console.error("API ERROR", error);
@@ -51,18 +45,36 @@ function getRouteData(startLocation, endLocation) {
 
 function displayRoute(routeData) {
   // Extract the coordinates from route data
-  var coordinates = routeData.routes[0].legs[0].points.map(function (
-    coordinate
-  ) {
+  var coordinates = routeData.routes[0].legs[0].points.map(function (coordinate) {
     // Transform each coordinate object to a Leaflet Latitude, Longitude object
     return L.latLng(coordinate.latitude, coordinate.longitude);
   });
 
-  // Create a route line from the coordinates and add it to the map
-  routeLine = L.polyline(coordinates, { color: "blue" }).addTo(map);
+  // Create a routeLine from the coordinates and add it to the map
+  routeLine = L.polyline(coordinates, { color: "blue" }).addTo(map).snakeIn();
+}
 
-  // Zoom the map to fit the routeLine
-  map.fitBounds(routeLine.getBounds());
+function onRouteClick(event) {
+  // If a route is already drawn, display a popup and exit the function
+  if (isRouteDrawn) {
+    L.popup().setLatLng(event.latlng).setContent("Please clear the route before adding a new one").openOn(map);
+    return;
+  }
+
+  // If the start location has not been set, set it to the clicked location and add a marker
+  if (!startLocation) {
+    startLocation = event.latlng.lat + "," + event.latlng.lng;
+    startMarker = L.marker(event.latlng).addTo(map);
+    return;
+  }
+
+  // If the start location has been set, set the end location to the clicked location and add a marker
+  endLocation = event.latlng.lat + "," + event.latlng.lng;
+  endMarker = L.marker(event.latlng).addTo(map);
+
+  getRouteData(startLocation, endLocation);
+
+  isRouteDrawn = true;
 }
 
 function displayCurrentLocation() {
@@ -81,6 +93,9 @@ function clearRoute() {
   map.removeLayer(routeLine);
   map.removeLayer(startMarker);
   map.removeLayer(endMarker);
+
+  // Remove popups from the map
+  map.closePopup();
 
   // Reset
   startLocation = null;
@@ -107,6 +122,15 @@ function createClearRouteButton() {
   }).addTo(map);
 }
 
+function displayRouteDetails(routeData) {
+  var routeDistance = routeData.routes[0].summary.lengthInMeters;
+  var routeDuration = routeData.routes[0].summary.travelTimeInSeconds;
+  var arrivalTime = routeData.routes[0].summary.arrivalTime;
+  var departureTime = routeData.routes[0].summary.departureTime;
+
+  // Data for route to display on our page, if someone gets the chance when the design is done
+}
+
 function trafficMap() {
   L.control
     .layers(
@@ -124,64 +148,22 @@ function trafficMap() {
     .addTo(map);
 }
 
-// || INITIALISE THE PAGE
-function init() {
-  displayMap();
-  displayCurrentLocation();
-  search();
-  createClearRouteButton();
-  trafficMap();
-
-
-  // Click event to draw routeLine on the map between a start and end location
-  map.on("click", function (event) {
-    // If a route is not drawn and a start location is not set, set the start location to the clicked location
-    if (!isRouteDrawn && !startLocation) {
-      startLocation = event.latlng.lat + "," + event.latlng.lng;
-      (startMarker = L.marker(event.latlng)).addTo(map);
-    } else if (!isRouteDrawn) {
-      // If a route is not drawn and a start location is set, set the end location to the clicked location
-      endLocation = event.latlng.lat + "," + event.latlng.lng;
-      endMarker = L.marker(event.latlng).addTo(map);
-
-      // Retrieve route data and display it on the map
-      getRouteData(startLocation, endLocation);
-
-      // Indicate that a route is drawn
-      isRouteDrawn = true;
-    } else {
-      // If a route is drawn, display a popup to inform the user to clear the route first
-      L.popup()
-        .setLatLng(event.latlng)
-        .setContent("Please clear the route before adding a new one")
-        .openOn(map);
-    }
-  });
-}
-
 // Search control within map
 function search() {
-
-var geocoder = L.Control.geocoder({
-  defaultMarkGeocode: false
-})
-  .on('markgeocode', function(e) {
-    var bbox = e.geocode.bbox;
-    var poly = L.polygon([
-      bbox.getSouthEast(),
-      bbox.getNorthEast(),
-      bbox.getNorthWest(),
-      bbox.getSouthWest()
-    ]).addTo(map);
-    map.fitBounds(poly.getBounds());
+  var geocoder = L.Control.geocoder({
+    defaultMarkGeocode: false,
   })
-  .addTo(map);
+    .on("markgeocode", function (e) {
+      var bbox = e.geocode.bbox;
+      var poly = L.polygon([bbox.getSouthEast(), bbox.getNorthEast(), bbox.getNorthWest(), bbox.getSouthWest()]).addTo(
+        map
+      );
+      map.fitBounds(poly.getBounds());
+    })
+    .addTo(map);
 }
 
-init();
-
 // AQI (Air Quality index API) CODE
-
 $.get(url + city + url2).then(function (currentData) {
   console.log(currentData);
 
@@ -207,3 +189,18 @@ $.get(url + city + url2).then(function (currentData) {
     $("#qualityBox").css({ backgroundColor: "red", color: "white" });
   }
 });
+
+// || INITIALISE THE PAGE
+function init() {
+  // Call Functions
+  displayMap();
+  displayCurrentLocation();
+  search();
+  createClearRouteButton();
+  trafficMap();
+
+  // Click Events
+  map.on("click", onRouteClick);
+}
+
+init();
